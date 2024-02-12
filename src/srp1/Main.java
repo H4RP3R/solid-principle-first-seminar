360ea24408adea3f1e97ea25fbfb28156840f702
zombiehunter <viktormallory25@gmail.com>
2024-02-12 12:03:56 +0300

    done

---
+++ src/srp1/Main.java
@@ -0,0 +1,16 @@
+package srp1;
+
+import srp1.draw.Drawer;
+import srp1.draw.SquareDrawer;
+import srp1.figure.Point;
+import srp1.figure.Square;
+
+public class Main {
+    public static void main(String[] args) {
+        Square square = new Square(new Point(1, 1), 5);
+        System.out.printf("Square area: %d \n", square.getArea());
+
+        Drawer drawer = new SquareDrawer();
+        drawer.draw(square.getSide());
+    }
+}
